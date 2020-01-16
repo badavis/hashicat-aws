d7bdc3ac54cb48bf539f064b930ff739bd84d2b8
Benjamin Davis <bjamin.davis@gmail.com>
2020-01-16 22:48:30 +0000

    Use actual required variables

--- vpc.tf
+++ vpc.tf
@@ -2,12 +2,18 @@ module "vpc" {
   source  = "app.terraform.io/ben-training/vpc/aws"
   version = "2.22.0"
 
-  database_subnet_assign_ipv6_address_on_creation = false
-  elasticache_subnet_assign_ipv6_address_on_creation = false
-  enable_classiclink = false
-  enable_classiclink_dns_support = false
-  intra_subnet_assign_ipv6_address_on_creation = false
-  private_subnet_assign_ipv6_address_on_creation = false
-  public_subnet_assign_ipv6_address_on_creation = false
-  redshift_subnet_assign_ipv6_address_on_creation = false
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
